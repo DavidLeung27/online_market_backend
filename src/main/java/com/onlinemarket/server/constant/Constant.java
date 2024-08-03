ea7b355f813e1a3f0f3e89fa76564d9e66ab1fab
David Leung <leungkwinglok@gmail.com>
2024-08-03 16:52:29 +0800

    fix google login bug

--- src/main/java/com/onlinemarket/server/constant/Constant.java
+++ src/main/java/com/onlinemarket/server/constant/Constant.java
@@ -1,15 +1,23 @@
-package com.onlinemarket.server.user;
+package com.onlinemarket.server.constant;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+
+// import com.google.api.client.util.Value;
 
 import lombok.Data;
 
 @Component
 @ConfigurationProperties(prefix = "google.client")
 @Data
-public class GoogleInfo {
+public class Constant {
+    // @Value("${google.client.id}")
     private String id;
+
+    // @Value("${google.client.secret}")
     private String secret;
+
+    // @Value("${google.client.redirect}")
+    private String redirect;
 
 }
